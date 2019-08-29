5f4d990615be308c5da6b0576b623dd6437aaba4
ikesh pack <packappsforyou@gmail.com>
2019-08-29 12:46:07 -0400

    Add nav

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import ProductIndex from "./products/ProductsIndex";
 import AuthRoute from "./AuthRoute";
 import Login from "./Login";
+import Nav from "./Nav";
 
 const App = () => {
   return (
     <div>
       <h1>Online Store</h1>
+      <Nav />
       <Switch>
         <AuthRoute exact path="/login" component={Login} routeType="auth" />
         <Route path="/" component={ProductIndex} />
