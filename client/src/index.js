30b8ebbf5a8e1f6ad038b4ad9fc2618ee2b6a9c6
ikesh pack <packappsforyou@gmail.com>
2019-08-29 11:56:58 -0400

    Restructuring

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
