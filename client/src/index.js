3788eb9d5a9c3c9be86290f94d9879564b5eab0f
ikesh pack <packappsforyou@gmail.com>
2019-08-29 11:42:52 -0400

    Client setup

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
