39f456686e68a9ef49338789993fc6640b50f9b0
ikesh pack <packappsforyou@gmail.com>
2019-08-29 11:45:25 -0400

    Index setup

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,42 @@ import { ApolloProvider } from "react-apollo";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object._id || null
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000/graphql"
+});
+
+// make sure we log any additional errors we receive
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, httpLink]),
+  cache
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  }
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
